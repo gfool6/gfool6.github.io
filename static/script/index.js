e35f0da2449d171eb6f7eb08fd8c9717b2b26014
gfool6 <39181825+gfool6@users.noreply.github.com>
2022-10-14 17:37:47 +0900

    Add files via upload

---
+++ static/script/index.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+    initialize();
+};
+
+function initialize(){
+    const hamburgerButton = document.querySelector("button.hamburger");
+    console.log(hamburgerButton);
+    hamburgerButton.addEventListener('click', ev => {
+        const menu = document.querySelector(".slidemenu");
+        console.log(getComputedStyle(menu).opacity);
+        if(getComputedStyle(menu).opacity == 0){
+            menu.classList.add("fade-in-rtl");
+            menu.classList.remove("fade-in-ltr");
+        }
+        else{
+            menu.classList.add("fade-in-ltr");
+            menu.classList.remove("fade-in-rtl");
+        }
+    });
+}

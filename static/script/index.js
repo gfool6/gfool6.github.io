968be23484603981d9316bb3908f3f65b8923210
g <>
2022-10-18 11:53:22 +0900

    hamburger

--- static/script/index.js
+++ static/script/index.js
@@ -4,17 +4,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function initialize(){
     const hamburgerButton = document.querySelector("button.hamburger");
-    console.log(hamburgerButton);
+    
     hamburgerButton.addEventListener('click', ev => {
         const menu = document.querySelector(".slidemenu");
-        console.log(getComputedStyle(menu).opacity);
-        if(getComputedStyle(menu).opacity == 0){
-            menu.classList.add("fade-in-rtl");
+        ev.preventDefault();
+        if(hamburgerButton.classList.contains("onshow")){
+            
             menu.classList.remove("fade-in-ltr");
+            requestAnimationFrame(function () {
+                menu.classList.add("fade-in-rtl");
+            });
+
+            hamburgerButton.classList.remove("onshow");
+            requestAnimationFrame(function () {
+                hamburgerButton.classList.add("onclose");
+            });
         }
         else{
-            menu.classList.add("fade-in-ltr");
             menu.classList.remove("fade-in-rtl");
+            requestAnimationFrame(function () {
+                menu.classList.add("fade-in-ltr");
+            });
+
+            hamburgerButton.classList.remove("onclose");
+            requestAnimationFrame(function () {
+                hamburgerButton.classList.add("onshow");
+            });
+            
         }
     });
 }

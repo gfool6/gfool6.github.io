d27a645e10b85e309a1849ded8097a5bd671e000
g <>
2022-10-19 18:51:27 +0900

    directory page other

--- static/script/index.js
+++ static/script/index.js
@@ -1,10 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    initialize();
+    initHamburger();
+    initTransition();
 });
 
-function initialize(){
+function initTransition(){
+    document.querySelector("#transition-picture").addEventListener('click', ev => {
+        location.href = "/pages/picture.html"
+    });
+    document.querySelector("#transition-3d").addEventListener('click', ev => {
+        location.href = "/pages/3dmodel.html"
+    });
+    document.querySelector("#transition-software").addEventListener('click', ev => {
+        location.href = "/pages/software.html"
+    });
+    document.querySelector("#transition-social").addEventListener('click', ev => {
+        location.href = "/pages/social.html"
+    });
+    document.querySelector("#transition-blog").addEventListener('click', ev => {
+        location.href = "/pages/blog.html"
+    });
+}
+
+function initHamburger(){
     const hamburgerButton = document.querySelector("button.hamburger");
-    
     hamburgerButton.addEventListener('click', ev => {
         const menu = document.querySelector(".slidemenu");
         ev.preventDefault();

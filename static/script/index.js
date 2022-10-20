c27bf579e732e01fed7026c49282859fc4531f57
g <>
2022-10-20 11:04:28 +0900

    transition, pageview

--- static/script/index.js
+++ static/script/index.js
@@ -1,24 +1,48 @@
+const indexViewPath = "/pages/indexContent.html";
+const pictureViewPath = "/pages/picture.html";
+const modelViewPath = "/pages/3dmodel.html";
+const softwareViewPath = "/pages/software.html";
+const socialViewPath = "/pages/social.html";
+const blogViewPath = "/pages/blog.html";
+
+
 document.addEventListener('DOMContentLoaded', function() {
     initHamburger();
     initTransition();
+    initContent(indexViewPath);
 });
+
+function initContent(loadingFile){
+    const contentArea = document.querySelector("#content-area");
+    fetch(loadingFile, {
+        referrerPolicy: "origin",
+        mode: "cors",
+    })
+    .then(response => response.text())
+    .then(t => {
+        contentArea.innerHTML = t;
+    });
+}   
 
 function initTransition(){
     document.querySelector("#transition-picture").addEventListener('click', ev => {
-        location.href = "/pages/picture.html"
+        initContent(pictureViewPath);
     });
     document.querySelector("#transition-3d").addEventListener('click', ev => {
-        location.href = "/pages/3dmodel.html"
+        initContent(modelViewPath);
     });
     document.querySelector("#transition-software").addEventListener('click', ev => {
-        location.href = "/pages/software.html"
+        initContent(softwareViewPath);
     });
     document.querySelector("#transition-social").addEventListener('click', ev => {
-        location.href = "/pages/social.html"
+        initContent(socialViewPath);
     });
     document.querySelector("#transition-blog").addEventListener('click', ev => {
-        location.href = "/pages/blog.html"
+        initContent(blogViewPath);
     });
+    document.querySelector("header>row>h1").addEventListener('click', ev => {
+        initContent(indexViewPath);
+    });;
 }
 
 function initHamburger(){
